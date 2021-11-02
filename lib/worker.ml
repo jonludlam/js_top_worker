b6ddbcc39c9b7ca888d42304961153b619d6971b
Jon Ludlam <jon@recoil.org>
2021-11-02 22:52:23 +0000

    Many fixes

--- lib/worker.ml
+++ lib/worker.ml
@@ -55,8 +55,9 @@ let exec' s =
   let res : bool = JsooTop.use Format.std_formatter s in
   if not res then Format.eprintf "error while evaluating %s@." s
 
-let setup () =
+let setup functions () =
   JsooTop.initialize ();
+  List.iter (fun f -> f ()) functions;
   Sys.interactive := false;
   if Version.compare Version.current [ 4; 07 ] >= 0 then exec' "open Stdlib";
   let header1 = Printf.sprintf "        %s version %%s" "OCaml" in
@@ -129,19 +130,22 @@ let execute =
         ; highlight = !highlighted
         }
 
-let setup () =
-  Js_of_ocaml.Sys_js.set_channel_flusher stdout (Buffer.add_string stdout_buff);
-  Js_of_ocaml.Sys_js.set_channel_flusher stderr (Buffer.add_string stderr_buff);
-  setup ();
-  setup_printers ();
-  IdlM.ErrM.return
-    Toplevel_api_gen.
-      { stdout = buff_opt stdout_buff
-      ; stderr = buff_opt stderr_buff
-      ; sharp_ppf = None
-      ; caml_ppf = None
-      ; highlight = None
-      }
+let setup functions () =
+  try
+    Js_of_ocaml.Sys_js.set_channel_flusher stdout (Buffer.add_string stdout_buff);
+    Js_of_ocaml.Sys_js.set_channel_flusher stderr (Buffer.add_string stderr_buff);
+    setup functions ();
+    setup_printers ();
+    IdlM.ErrM.return
+      Toplevel_api_gen.
+        { stdout = buff_opt stdout_buff
+        ; stderr = buff_opt stderr_buff
+        ; sharp_ppf = None
+        ; caml_ppf = None
+        ; highlight = None
+        }
+  with e ->
+    IdlM.ErrM.return_err (Toplevel_api_gen.InternalError (Printexc.to_string e))
 
 let complete phrase =
   let contains_double_underscore s =
@@ -168,13 +172,68 @@ let server process e =
   M.bind (process call) (fun response -> Js_of_ocaml.Worker.post_message (Marshal.to_string response []));
   ()
 
-let run files =
+  let sync_get url =
+    let open Js_of_ocaml in
+    let x = XmlHttpRequest.create () in
+    x##.responseType := (Js.string "arraybuffer");
+    x##_open (Js.string "GET") (Js.string url) Js._false;
+    x##send Js.null;
+    match x##.status with
+    | 200 ->
+      Js.Opt.case
+        (File.CoerceTo.arrayBuffer x##.response)
+        (fun () ->
+          Firebug.console##log (Js.string "Failed to receive file");
+          None)
+        (fun b ->
+          Some (Typed_array.String.of_arrayBuffer b))
+    | _ ->
+      None
+
+let load_resource files =
+  let open Js_of_ocaml in
+  fun ~prefix ~path ->
+    Firebug.console##log (Js.string (Printf.sprintf "here we are, loading prefix=%s path=%s" prefix path));
+    (* let abs_filename = Filename.concat prefix path in *)
+    if List.mem_assoc path files
+    then begin
+      Firebug.console##log (Js.string "path is in files");
+      let f = sync_get (List.assoc path files) in
+      match f with
+      | Some content ->
+        Firebug.console##log (Js.string (Printf.sprintf "Got result (length=%d)" (String.length content)));
+        (* Sys_js.update_file ~name:abs_filename ~content; *)
+        Some content
+      | None -> 
+        None
+      end
+    else
+      (Firebug.console##log (Js.string "path is NOT in files");
+      None)
+
+let run files cmis functions =
   (* Here we bind the server stub functions to the implementations *)
-  Js_top_worker_rpc.Idl.logfn := (fun s -> Js_of_ocaml.(Firebug.console##log ( s)));
-  Clflags.no_check_prims := true;
-  Server.complete complete;
-  Server.exec execute;
-  Server.setup setup;
-  let rpc_fn = IdlM.server Server.implementation in
-  Js_of_ocaml.Worker.import_scripts files;
-  Js_of_ocaml.Worker.set_onmessage (server rpc_fn)
+  let open Js_of_ocaml in
+  try
+    Js_top_worker_rpc.Idl.logfn := (fun s -> Js_of_ocaml.(Firebug.console##log ( s)));
+    ignore cmis;
+    Clflags.no_check_prims := true;
+    let cmi_files = List.map (fun cmi ->
+      (Filename.basename cmi, cmi)) cmis in
+    Sys_js.mount ~path:"/dynamic/cmis" (load_resource cmi_files);
+    List.iter (fun (path, _) -> Sys_js.register_lazy ("/dynamic/cmis/" ^ path)) cmi_files;
+    Topdirs.dir_directory "/dynamic/cmis";
+    Js_of_ocaml.Worker.import_scripts files;
+    let functions = List.map (fun func_name ->
+      Firebug.console##log (Js.string ("Function: " ^ func_name ));
+      let func = Js.Unsafe.js_expr func_name in
+      fun () -> Js.Unsafe.fun_call func [| Js.Unsafe.inject Dom_html.window |])
+      functions in
+    Server.complete complete;
+    Server.exec execute;
+    Server.setup (setup functions);
+    let rpc_fn = IdlM.server Server.implementation in
+    Js_of_ocaml.Worker.set_onmessage (server rpc_fn);
+    Firebug.console##log (Js.string "All finished");
+    with e ->
+      Firebug.console##log (Js.string ("Exception: " ^ Printexc.to_string e))
