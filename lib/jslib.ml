f975e085be0c54b1adfce84999f4333f214a3c56
Jon Ludlam <jon@recoil.org>
2025-06-12 12:04:19 +0100

    Allow remote js opam

--- lib/jslib.ml
+++ lib/jslib.ml
@@ -5,6 +5,16 @@ let log fmt =
 
 let sync_get url =
   let open Js_of_ocaml in
+  let global_rel_url =
+    let x : Js.js_string Js.t option = Js.Unsafe.js_expr "globalThis.__global_rel_url" |> Js.Optdef.to_option in
+    Option.map Js.to_string x
+  in
+  let url =
+    match global_rel_url with
+    | Some rel -> Filename.concat rel url
+    | None -> url
+  in
+  Console.console##log (Js.string ("Fetching: " ^ url));
   let x = XmlHttpRequest.create () in
   x##.responseType := Js.string "arraybuffer";
   x##_open (Js.string "GET") (Js.string url) Js._false;
